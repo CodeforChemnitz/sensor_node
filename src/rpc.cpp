12ad314b0341f6da95dbaa1382ce6c5eeda2c7d6
PhiBo <phibo@dinotools.org>
2015-11-12 22:15:31 +0100

    getUUID

--- src/rpc.cpp
+++ src/rpc.cpp
@@ -107,6 +107,22 @@ uint8_t ArduRPC_SensorNode::call(uint8_t cmd_id)
     return RPC_RETURN_SUCCESS;
   } else if (cmd_id == 0x21) {
     /* getUUID() */
+    eeprom_pos = NODE_EEPROM_UUID_OFFSET;
+    len = EEPROM.read(eeprom_pos);
+    eeprom_pos++;
+
+    if(len > 64) {
+      len = 64;
+    }
+
+    for(i = 0; i < len; i++) {
+      s[i] = EEPROM.read(eeprom_pos);
+      eeprom_pos++;
+    }
+
+    this->_rpc->writeResult_string(s, len);
+
+    return RPC_RETURN_SUCCESS;
   } else if (cmd_id == 0x22) {
     /* getKey() */
   } else if (cmd_id == 0x23) {
